0b801595c2c1b078db2edf7047e404b1a2914eaa
JCamiloRamirezTabares <juancaram21@gmail.com>
2022-09-27 23:10:10 -0500

    First Commit

---
+++ .ammonite/scala-2.13.8/amm-2.5.4-13-1ebd00a6/src/main/scala/Main/src/ammonite/$file/src/main/scala/Main.scala
@@ -0,0 +1,50 @@
+
+package ammonite
+package $file.src.main.scala
+import _root_.ammonite.interp.api.InterpBridge.{
+  value => interp
+}
+import _root_.ammonite.interp.api.InterpBridge.value.{
+  exit,
+  scalaVersion
+}
+import _root_.ammonite.interp.api.IvyConstructor.{
+  ArtifactIdExt,
+  GroupIdExt
+}
+import _root_.ammonite.compiler.CompilerExtensions.{
+  CompilerInterpAPIExtensions,
+  CompilerReplAPIExtensions
+}
+import _root_.ammonite.runtime.tools.{
+  browse,
+  grep,
+  time,
+  tail
+}
+import _root_.ammonite.compiler.tools.{
+  desugar,
+  source
+}
+import _root_.mainargs.{
+  arg,
+  main
+}
+import _root_.ammonite.repl.tools.Util.{
+  PathRead
+}
+
+
+object Main{
+/*<script>*/object Main{
+
+    def main(args: Array[String]):Unit = {
+        print("Hola Mundo")
+    }
+
+
+}/*</script>*/ /*<generated>*/
+def $main() = { scala.Iterator[String]() }
+  override def toString = "Main"
+  /*</generated>*/
+}
